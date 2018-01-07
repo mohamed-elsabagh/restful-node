94a0d74c9c3e88c523d872eb10699ae4e4ed8de8
Mohamed El-Sabagh <mohamed.elsabagh88@gmail.com>
2018-01-07 23:53:15 +0200

    updated test route

--- routes/testRoute.js
+++ routes/testRoute.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var logController = require('../controllers/logController');
 
 // Create our Express router
 var router = express.Router();
@@ -7,11 +8,14 @@ var router = express.Router();
 /*       Test Routes            */
 /********************************/
 // Create endpoint handlers for /api/test
+// router.route('/')
+//     .get(function(req, res) {
+//         res.json({
+//             message: 'Testing route is working'
+//         });
+//     });
+
 router.route('/')
-    .get(function(req, res) {
-        res.json({
-            message: 'Testing route is working'
-        });
-    });
+    .get(logController.getLastLog);
 
 module.exports = router;
